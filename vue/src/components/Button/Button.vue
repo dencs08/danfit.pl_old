3b36733171e9528457f3585b44c1008d853dc94e
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-20 23:05:57 +0100

    add: Button component

---
+++ vue/src/components/Button/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="px-16 py-5 rounded-full bg-primaryColor text-primaryWhite font-semibold">
+        <slot></slot>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ vue/src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Button } from "./Button/Button.vue";
+export { default as Navbar } from "./Navbar/Navbar.vue";
