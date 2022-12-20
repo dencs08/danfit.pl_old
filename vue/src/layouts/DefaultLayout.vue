836c1d57c34b36387e78ea8557577c5c607e18c8
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-20 23:07:54 +0100

    add: Default, Admin layouts

---
+++ vue/src/layouts/DefaultLayout.vue
@@ -0,0 +1,36 @@
+<template>
+    <Navbar />
+    <section class="overflow-hidden relative">
+        <img class="opacity-[35%] absolute z-[-99] min-w-[150vw] blur-md" src="../assets/logo.svg" alt="" />
+
+        <router-view v-slot="{ Component }">
+            <transition name="fade" mode="out-in">
+                <component :is="Component" />
+            </transition>
+        </router-view>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import { Navbar } from '../components'
+
+export default defineComponent({
+    components: {
+        Navbar
+    }
+})
+</script>
+
+<style lang="scss">
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.250s ease-out;
+}
+</style>
