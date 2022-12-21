246257f246cc1e42d4a4fa42d756d177b2515df9
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-21 19:10:07 +0100

    add: Footer container

--- vue/src/components/Button/Button.vue
+++ vue/src/components/Button/Button.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="px-16 py-5 rounded-full bg-primaryColor text-primaryWhite font-semibold">
+    <button
+        class="w-full xs:w-auto px-0 xs:px-8 md:px-10 lg:px-10 py-3 lg:py-4 rounded-full bg-primaryColor hover:bg-secondaryColor transtion-all duration-200 text-primaryWhite font-semibold">
         <slot></slot>
     </button>
 </template>
---
+++ vue/src/containers/Footer/Footer.vue
@@ -0,0 +1,100 @@
+<template>
+    <footer class="pt-14 pb-8 bg-gray-900">
+        <div class="container md:flex md:justify-between space-x-10">
+            <div class="mb-24 md:mb-0">
+                <router-link to="/start" class="flex items-center">
+                    <img src="../../assets/logos/danfit_logo_white.svg" class="mr-3 h-8" alt="danfit Logo" />
+                </router-link>
+
+                <div class="mt-8 ">
+                    <p class="text-white">Zapisz się do naszego newstellera</p>
+                    <p class="text-gray-500">(informacje o eventach np. turnieje)</p>
+
+                    <div class="mt-4">
+                        <form action="#">
+                            <input type="text" id="email"
+                                class="border text-sm rounded-full block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                                placeholder="Email" required>
+                            <Button class="mt-2 float-right">Zapisz</Button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-2 gap-8 sm:gap-16 sm:grid-cols-3">
+                <div>
+                    <h2 class="mb-6 text-sm font-semibold uppercase text-white">Menu</h2>
+                    <ul class="text-gray-400 space-y-4">
+                        <li v-for="link in menu">
+                            <router-link :to="link.to" class="hover:underline">{{ link.name }}</router-link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="mb-6 text-sm font-semibold uppercase text-white">Kontakt</h2>
+                    <ul class="text-gray-400 space-y-4">
+                        <li v-for="el in contact">
+                            <span>{{ el.name }}</span>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 class="mb-6 text-sm font-semibold uppercase text-white">O Stronie</h2>
+                    <ul class="text-gray-400 space-y-4">
+                        <li v-for="link in privacy">
+                            <router-link :to="link.to" class="hover:underline">{{ link.name }}</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr class="container my-6  sm:mx-auto border-gray-700 lg:my-8" />
+        <div class="container flex flex-col sm:flex-row items-center sm:justify-between">
+            <span class="text-sm  sm:text-center text-gray-400">© 2022
+                <router-link to="/start" class="hover:underline">
+                    danfit
+                </router-link>. Wszelkie prawa zastrzeżone.
+            </span>
+            <div class="flex mt-4 space-x-6 justify-center sm:mt-0">
+                <a href="https://www.facebook.com/danfitpl" target="_blank" rel="noopener"
+                    class="text-gray-500 hover:text-white">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <path fill-rule="evenodd"
+                            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <span class="sr-only">Facebook page</span>
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
+<script lang="ts">
+import { Button } from '../../components';
+export default {
+    components: {
+        Button
+    },
+
+    data() {
+        return {
+            menu: [
+                { name: 'Start', to: '#' },
+                { name: 'Oferta', to: '#' },
+                { name: 'Dlaczego my', to: '#' },
+                { name: 'Zarezerwuj trening', to: '#' },
+            ],
+            contact: [
+                { name: 'biuro@danfit.pl', to: '#' },
+                { name: '882 031 130', to: '#' },
+            ],
+            privacy: [
+                { name: 'Polityka prywatności', to: '#' },
+                { name: 'Warunki korzystania', to: '#' },
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
