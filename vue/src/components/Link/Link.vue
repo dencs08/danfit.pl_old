fcda635160c8de64d0efcdf706d43fcbf6412de0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-21 19:10:13 +0100

    add: Link component

---
+++ vue/src/components/Link/Link.vue
@@ -0,0 +1,17 @@
+<template>
+    <router-link :to="link">
+        <span class="text-secondaryColor underline">
+            <slot></slot>
+        </span>
+    </router-link>
+</template>
+<script lang="ts">
+export default {
+    props: {
+        link: String,
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
