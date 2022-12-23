87357afef792f1ed5c24933dfc73389a7555d4d3
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-24 00:17:51 +0100

    add: Calendar view

---
+++ vue/src/views/Calendar.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <Calendar class="section-pb" />
+        <Footer />
+    </main>
+</template>
+<script lang="ts">
+import {
+    Calendar,
+    Footer
+} from '../containers';
+export default {
+    components: {
+        Calendar,
+        Footer
+    }
+}
+</script>
+<style lang="">
+    
+</style>
