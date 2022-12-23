0cecf9790c9b4e191031db83ba3bd4249b6e67ea
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-23 13:10:15 +0100

    add: TextUnderline component

---
+++ vue/src/components/TextUnderline/TextUnderline.vue
@@ -0,0 +1,15 @@
+<template>
+    <span class="underline underline-offset-3 decoration-8 decoration-secondaryColor dark:decoration-emerald-400">
+        <slot></slot>
+    </span>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
