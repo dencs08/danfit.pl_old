0b5c823aa14ad40b6c61b27abb03ed010e4cde54
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-23 13:10:56 +0100

    add: WhyUs container

---
+++ vue/src/containers/WhyUs/WhyUs.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="text-center">
+        <div class="container">
+            <h3 class="mb-6">Postaw na nas.</h3>
+            <div class="grid row-gap-8 sm:grid-cols-3 space-y-8 sm:space-y-0">
+                <div class="text-center">
+                    <h6 class="font-bold text-primaryColor">13+</h6>
+                    <p class="font-bold">Lat doświadczenia</p>
+                </div>
+                <div class="text-center">
+                    <h6 class="font-bold text-primaryColor">110+</h6>
+                    <p class="font-bold">Zadowolonych klientów</p>
+                </div>
+                <div class="text-center">
+                    <h6 class="font-bold text-primaryColor">2750+</h6>
+                    <p class="font-bold">Wykonanych treningów</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script lang="ts">
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+h6 {
+    font-size: clamp(1.2rem, 3vw, 4rem);
+}
+</style>
