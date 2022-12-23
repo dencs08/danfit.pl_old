1863de8735e371084c4479f3e67e4d18cc8d82f8
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-23 13:10:00 +0100

    add: TextAreaCustom component

---
+++ vue/src/components/TextAreaCustom.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="sm:col-span-2">
+        <div class="flex justify-between">
+            <label :for="name" class="block text-sm font-medium">{{ label }} <span v-if="required">*</span></label>
+            <span id="message-max" class="text-sm">do 500 znaków</span>
+        </div>
+        <div class="mt-1">
+            <textarea :id="name" :name="name" :rows="rows"
+                class="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-3 py-3"
+                aria-describedby="message-max" :placeholder="placeholder" />
+        </div>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    props: {
+        label: String,
+        name: String,
+        placeholder: String,
+        rows: Number,
+        required: Boolean,
+
+        optional: {
+            default: false,
+            type: Boolean,
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
