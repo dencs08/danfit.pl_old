19ae2374b68f5bf4a6fe46221020e6a0280c3a3d
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-23 13:10:08 +0100

    add: TextGradient component

---
+++ vue/src/components/TextGradient/TextGradient.vue
@@ -0,0 +1,15 @@
+<template>
+    <span class="text-transparent bg-clip-text bg-gradient-to-r from-secondaryColor to-emerald-600">
+        <slot></slot>
+    </span>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
