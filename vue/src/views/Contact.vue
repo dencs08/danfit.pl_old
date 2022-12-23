01fa85268f4f61c5cd4353cee5ff8b614cf70c5b
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-24 00:17:57 +0100

    add: Contact view

---
+++ vue/src/views/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        <ContactForm class="section-pt" />
+        <FAQ class="section-pb section-pt bg-primaryWhite" />
+        <Footer />
+    </main>
+</template>
+
+<script lang="ts">
+import { ContactForm, FAQ, Footer } from '../containers';
+
+export default {
+    components: {
+        ContactForm,
+        FAQ,
+        Footer
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
