ffbcad01c768c707e688a5422ab0b92919812337
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-25 03:00:53 +0100

    add: Booking view

---
+++ vue/src/views/User/Booking.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h6>Rezerwacje</h6>
+        <p>Tu możesz dokonać rezerwacji na trening.</p>
+
+        <DayCalendar class="w-[110%]" />
+    </div>
+</template>
+<script lang="ts">
+import { DayCalendar } from '../../components';
+export default {
+    components: {
+        DayCalendar
+    }
+}
+</script>
+<style lang="">
+    
+</style>
