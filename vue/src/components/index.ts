1d4d164435ff768ad7d50e5c940a78f5fddd2303
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-25 03:00:33 +0100

    add: component exports

--- vue/src/components/index.ts
+++ vue/src/components/index.ts
@@ -1,2 +1,16 @@
+export { default as TextGradient } from "./TextGradient/TextGradient.vue";
+export { default as TextUnderline } from "./TextUnderline/TextUnderline.vue";
+export { default as Link } from "./Link/Link.vue";
 export { default as Button } from "./Button/Button.vue";
+export { default as InputCustom } from "./InputCustom.vue";
+export { default as TextAreaCustom } from "./TextAreaCustom.vue";
+
+export { default as Modal } from "./Modal.vue";
+
+export { default as AccountSidebar } from "./AccountSidebar.vue";
+
+export { default as MonthCalendar } from "./MonthCalendar.vue";
+export { default as DayCalendar } from "./DayCalendar.vue";
+
+export { default as Banner } from "./Banner/Banner.vue";
 export { default as Navbar } from "./Navbar/Navbar.vue";
