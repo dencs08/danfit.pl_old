7ef282086995a9fcce0ce418b4a81d2fc4f0128f
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-25 03:03:00 +0100

    add: views exports

--- vue/src/views/index.ts
+++ vue/src/views/index.ts
@@ -1,6 +1,18 @@
 export { default as Start } from "./Start.vue";
 export { default as Login } from "./Login.vue";
 export { default as Register } from "./Register.vue";
+export { default as Contact } from "./Contact.vue";
+export { default as Calendar } from "./Calendar.vue";
+export { default as Error404 } from "./Error404.vue";
+export { default as Privacy } from "./Privacy.vue";
+export { default as Terms } from "./Terms.vue";
+
 export { default as Dashboard } from "./Admin/Dashboard/Dashboard.vue";
 export { default as Overview } from "./Admin/Overview.vue";
 export { default as Clients } from "./Admin/Clients.vue";
+
+export { default as AccountSettings } from "./User/AccountSettings.vue";
+export { default as MyEvents } from "./User/MyEvents.vue";
+export { default as MyTrainings } from "./User/MyTrainings.vue";
+export { default as Booking } from "./User/Booking.vue";
+export { default as CoachTrainings } from "./User/CoachTrainings.vue";
