670260511d87c61f0499791f2bb934cbddd4412c
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-25 03:02:16 +0100

    add: Offer,WhyUs,Steps,AreYouReady,Footer in start view

--- vue/src/views/Start.vue
+++ vue/src/views/Start.vue
@@ -1,12 +1,24 @@
 <template>
-    <Hero />
+    <main>
+        <Hero />
+        <Offer id="offer" class="bg-gray-900 section-pt section-pb" />
+        <Steps id="steps" class="section-pt section-pb" />
+        <WhyUs id="whyUs" />
+        <AreYouReady class="section-pt" />
+        <Footer />
+    </main>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Hero } from '../containers';
+import { Hero, Offer, WhyUs, Steps, AreYouReady, Footer } from '../containers';
 export default defineComponent({
     components: {
         Hero,
+        Offer,
+        WhyUs,
+        Steps,
+        AreYouReady,
+        Footer
     }
 })
 </script>
