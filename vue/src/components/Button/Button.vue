493764282dbf01526d63afebca95662aba6e5dbb
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-25 02:59:53 +0100

    add: different btn sizes

--- vue/src/components/Button/Button.vue
+++ vue/src/components/Button/Button.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="w-full xs:w-auto px-0 xs:px-8 md:px-10 lg:px-10 py-3 lg:py-4 rounded-full bg-primaryColor hover:bg-secondaryColor transtion-all duration-200 text-primaryWhite font-semibold">
+        class="btn rounded-full bg-gradient-to-r from-secondaryColor to-emerald-600 hover:scale-[1.035] transtion-all duration-200 text-primaryWhite font-semibold">
         <slot></slot>
     </button>
 </template>
@@ -9,10 +9,41 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
+    mounted() {
 
+    },
+    methods: {
+
+    }
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.btn {
+    padding-top: clamp(1em, 0.9vw, 1.5em);
+    padding-right: clamp(1.75em, 2vw, 2.5em);
+    padding-bottom: clamp(1em, 0.9vw, 1.5em);
+    padding-left: clamp(1.75em, 2vw, 2.5em);
+}
 
+.btn-xs {
+    font-size: clamp(0.75rem, 0.7vw, 1.5rem);
+
+    padding-top: clamp(0.5em, 0.6vw, 1.5em);
+    padding-right: clamp(1.75em, 1.25vw, 2.5em);
+    padding-bottom: clamp(0.5em, 0.6vw, 1.5em);
+    padding-left: clamp(1.75em, 1.25vw, 2.5em);
+}
+
+.btn-sm {
+    font-size: clamp(0.75rem, 0.8vw, 1.5rem);
+}
+
+.btn-md {
+    font-size: clamp(0.8rem, 0.9vw, 2rem) !important;
+}
+
+.btn-lg {
+    font-size: clamp(0.85rem, 1vw, 2.5rem) !important;
+}
 </style>
