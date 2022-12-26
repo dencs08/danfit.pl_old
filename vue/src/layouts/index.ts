8ceb2127dfb34b687590b407a81ee44763df4232
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-26 20:01:28 +0100

    add: layout export

--- vue/src/layouts/index.ts
+++ vue/src/layouts/index.ts
@@ -1,2 +1,3 @@
 export { default as DefaultLayout } from "./DefaultLayout.vue";
 export { default as AdminLayout } from "./AdminLayout.vue";
+export { default as AccountLayout } from "./AccountLayout.vue";
