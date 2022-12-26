554b0bc3bce8e562b164d4e248a6fa6a5f52c7bb
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-26 20:01:20 +0100

    add: autoAnimatePlugin in main.ts

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
+import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 import router from "./router";
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(autoAnimatePlugin).mount("#app");
