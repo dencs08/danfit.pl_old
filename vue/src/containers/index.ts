425abb07afe06103dfc51f25f96c798a9481f7e0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-26 20:02:24 +0100

    update: Containers exports

--- vue/src/containers/index.ts
+++ vue/src/containers/index.ts
@@ -1,1 +1,12 @@
 export { default as Hero } from "./Hero/Hero.vue";
+export { default as WhyUs } from "./WhyUs/WhyUs.vue";
+export { default as Offer } from "./Offer/Offer.vue";
+export { default as Steps } from "./Steps/Steps.vue";
+export { default as FAQ } from "./FAQ/FAQ.vue";
+export { default as AreYouReady } from "./AreYouReady/AreYouReady.vue";
+export { default as ContactForm } from "./ContactForm/ContactForm.vue";
+export { default as Footer } from "./Footer/Footer.vue";
+
+export { default as AccountHeader } from "./AccountHeader.vue";
+
+export { default as Calendar } from "./Calendar.vue";
