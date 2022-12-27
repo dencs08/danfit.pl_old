3e8ede3401291bdc8fe3ca4eae381f4b6d07c76f
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-12-27 14:21:58 +0100

    update: Hero background illustrations layout & centered

--- vue/src/containers/Hero/Hero.vue
+++ vue/src/containers/Hero/Hero.vue
@@ -1,36 +1,60 @@
 <template>
-    <section class="">
-        <div class="container mx-auto">
-            <div class="flex flex-col justify-center h-screen">
+    <section class="relative">
+        <div class="max-w-[90%] xs:max-w-[100%] xs:px-[18vw] mx-auto">
+            <div class="md:h-screen pt-[12vh] pb-[8vh] md:py-[0] text-center grid place-content-center">
                 <div>
-                    <h1 class="italic">{{ title }}</h1>
+                    <h1>
+                        Akademia squasha <TextGradient>danfit</TextGradient>
+                    </h1>
 
-                    <div class="md:max-w-[60%]">
-                        <h2 class="mb-14">{{ subtitle }}</h2>
+                    <div class="mt-6 mb-8 sm:mb-14 font-color-secondary">
+                        <h2>
+                            Najlepsze treningi squasha w Zielonej Górze,
+                            <span>
+                                <Link link="Calendar">zapisz się</Link>
+                            </span>
+                            i zacznij trenować z nami!
+                        </h2>
                     </div>
 
-                    <router-link to="/login">
-                        <Button>{{ buttonText }}</Button>
+                    <router-link :to="{ name: 'Calendar' }">
+                        <Button class="w-full sm:w-auto btn-lg">{{ buttonText }}</Button>
                     </router-link>
                 </div>
             </div>
         </div>
+
+        <img :src="racket"
+            class="absolute top-[35%] xs:top-[70%] xl:top-[72.5%] left-[15%] w-[45vw] rotate-[90deg] xs:-rotate-[30deg] min-w-[250px] xs:min-w-[400px] md:min-w-[700px] opacity-40 xs:opacity-100 z-[-1] translate-x-[-50%] translate-y-[-50%]">
+        <img :src="racket"
+            class="absolute top-[70%] xs:top-[70%] xl:top-[72.5%] left-[85%] w-[45vw] rotate-[-90deg] xs:rotate-[30deg] min-w-[250px] xs:min-w-[400px] md:min-w-[700px] opacity-40 xs:opacity-100 z-[-1] translate-x-[-50%] translate-y-[-50%]">
+        <img :src="ball"
+            class="absolute top-[30%] lg:top-[20%] left-[5%] w-[5vw] blur-[1px] md:blur-[1px] rotate-45 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden xs:block">
+        <img :src="ball"
+            class="absolute top-[27.5%] lg:top-[20%] left-[86%] w-[8vw] blur-[1px] md:blur-[4px] rotate-90 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden xs:block">
+        <img :src="ball"
+            class="absolute top-[70%] lg:top-[80%] left-[22%] w-[4vw] blur-[1px] md:blur-[2px] rotate-0 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden sm:block">
     </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import { Button } from '../../components'
+import racket from '../../assets/hero/racket.png';
+import ball from '../../assets/hero/ball_white.png';
+
+import { Button, Link, TextGradient } from '../../components'
 export default defineComponent({
     components: {
-        Button
+        Button,
+        Link,
+        TextGradient
     },
     data() {
         return {
-            title: 'danfit',
-            subtitle: 'test',
-            buttonText: 'test'
+            racket: racket,
+            ball: ball,
+            buttonText: 'Zarezerwuj trening'
         }
     },
 })
