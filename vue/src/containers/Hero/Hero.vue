4fa38cba18d2675f971b3e8a531340166162e8ec
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-03 17:17:39 +0100

    update: img in Hero select-none

--- vue/src/containers/Hero/Hero.vue
+++ vue/src/containers/Hero/Hero.vue
@@ -25,15 +25,15 @@
         </div>
 
         <img :src="racket"
-            class="absolute top-[35%] xs:top-[70%] xl:top-[72.5%] left-[15%] w-[45vw] rotate-[90deg] xs:-rotate-[30deg] min-w-[250px] xs:min-w-[400px] md:min-w-[700px] opacity-40 xs:opacity-100 z-[-1] translate-x-[-50%] translate-y-[-50%]">
+            class="absolute top-[35%] xs:top-[70%] xl:top-[72.5%] left-[15%] w-[45vw] rotate-[90deg] xs:-rotate-[30deg] min-w-[250px] xs:min-w-[400px] md:min-w-[700px] opacity-40 xs:opacity-100 z-[-1] translate-x-[-50%] translate-y-[-50%] select-none">
         <img :src="racket"
-            class="absolute top-[70%] xs:top-[70%] xl:top-[72.5%] left-[85%] w-[45vw] rotate-[-90deg] xs:rotate-[30deg] min-w-[250px] xs:min-w-[400px] md:min-w-[700px] opacity-40 xs:opacity-100 z-[-1] translate-x-[-50%] translate-y-[-50%]">
+            class="absolute top-[70%] xs:top-[70%] xl:top-[72.5%] left-[85%] w-[45vw] rotate-[-90deg] xs:rotate-[30deg] min-w-[250px] xs:min-w-[400px] md:min-w-[700px] opacity-40 xs:opacity-100 z-[-1] translate-x-[-50%] translate-y-[-50%] select-none">
         <img :src="ball"
-            class="absolute top-[30%] lg:top-[20%] left-[5%] w-[5vw] blur-[1px] md:blur-[1px] rotate-45 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden xs:block">
+            class="absolute top-[30%] lg:top-[20%] left-[5%] w-[5vw] blur-[1px] md:blur-[1px] rotate-45 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden xs:block select-none">
         <img :src="ball"
-            class="absolute top-[27.5%] lg:top-[20%] left-[86%] w-[8vw] blur-[1px] md:blur-[4px] rotate-90 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden xs:block">
+            class="absolute top-[27.5%] lg:top-[20%] left-[86%] w-[8vw] blur-[1px] md:blur-[4px] rotate-90 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden xs:block select-none">
         <img :src="ball"
-            class="absolute top-[70%] lg:top-[80%] left-[22%] w-[4vw] blur-[1px] md:blur-[2px] rotate-0 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden sm:block">
+            class="absolute top-[70%] lg:top-[80%] left-[22%] w-[4vw] blur-[1px] md:blur-[2px] rotate-0 z-[-1] translate-x-[-50%] translate-y-[-50%] hidden sm:block select-none">
     </section>
 </template>
 
