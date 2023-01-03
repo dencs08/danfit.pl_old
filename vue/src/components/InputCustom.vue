860b52c2bdbabf791daf583592c7f43e36cd721b
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-03 17:19:01 +0100

    update: InputCustom responsivness

--- vue/src/components/InputCustom.vue
+++ vue/src/components/InputCustom.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="mt-1">
             <input :type="type" :name="name" :id="name"
-                class="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-3 py-3"
+                class="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-xs sm:text-sm p-2 sm:p-3"
                 :placeholder="placeholder" :aria-describedby="name + '-optional'" />
         </div>
     </div>
