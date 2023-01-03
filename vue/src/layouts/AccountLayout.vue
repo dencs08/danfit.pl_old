7abdd55f773a4fec042b1a589f469ef234e77b66
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-01-03 17:18:14 +0100

    remove: overflow-hidden from AccountLayout

--- vue/src/layouts/AccountLayout.vue
+++ vue/src/layouts/AccountLayout.vue
@@ -6,7 +6,7 @@
             <AccountHeader />
             <div class="relative -mt-32">
                 <div class="mx-auto max-w-screen-2xl px-4 pb-6 sm:px-6 lg:px-8 lg:pb-16">
-                    <div class="overflow-hidden rounded-lg bg-white shadow">
+                    <div class="rounded-lg bg-white shadow">
                         <div class="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-y-0 lg:divide-x">
                             <AccountSidebar />
                             <div class="py-6 px-4 sm:p-6 lg:pb-8 lg:col-span-10">
