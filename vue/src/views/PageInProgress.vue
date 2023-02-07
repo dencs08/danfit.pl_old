409103a8d239800482b3e0cc5b1ebc36d6856cce
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-02-07 12:56:01 +0100

    add: PageInProgress view

---
+++ vue/src/views/PageInProgress.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h-screen grid place-content-center text-center">
+        <h3 class="mb-4">Podstrona <TextGradient>aktualnie w budowie!</TextGradient>
+        </h3>
+        <Link link="Start" :underline="true">Powrót na strone główną</Link>
+    </div>
+</template>
+<script lang="ts">
+import { TextGradient, Link } from '../components'
+export default {
+    components: {
+        TextGradient,
+        Link
+    }
+}
+</script>
+<style lang="">
+    
+</style>
