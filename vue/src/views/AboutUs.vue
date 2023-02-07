600c2c638d750a7ec62e9c953aeca6a416422da4
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-02-07 12:55:48 +0100

    add: AboutUs view

---
+++ vue/src/views/AboutUs.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mt-24">
+        <h3>O nas</h3>
+    </div>
+</template>
+<script lang="ts">
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
