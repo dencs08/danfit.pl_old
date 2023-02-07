4a07bc733144eee46ecb33d64862c0bce2bcaaf2
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-02-07 22:38:42 +0100

    add: binding functionality in input and textarea compoentns

--- vue/src/components/TextAreaCustom.vue
+++ vue/src/components/TextAreaCustom.vue
@@ -5,7 +5,7 @@
             <span id="message-max" class="text-sm">do 500 znaków</span>
         </div>
         <div class="mt-1">
-            <textarea :id="name" :name="name" :rows="rows"
+            <textarea :id="name" :name="name" :rows="rows" v-model="value"
                 class="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-3 py-3"
                 aria-describedby="message-max" :placeholder="placeholder" />
         </div>
@@ -20,11 +20,30 @@ export default {
         rows: Number,
         required: Boolean,
 
+        modelValue: String,
+
         optional: {
             default: false,
             type: Boolean,
         }
-    }
+    },
+
+    data() {
+        return {
+            content: this.value
+        }
+    },
+
+    computed: {
+        value: {
+            get() {
+                return this.modelValue
+            },
+            set(value: any) {
+                this.$emit('update:modelValue', value)
+            }
+        }
+    },
 }
 </script>
 <style lang="">
