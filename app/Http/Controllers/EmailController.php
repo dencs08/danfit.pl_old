c8a600e25e7ba2b130ef57f6f1240ee14554d081
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-02-08 15:48:33 +0100

    add: EmailController send() function

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\EmailContactForm;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
-    //
+    public function send(Request $request)
+    {
+        $email = $request->email;
+        // return $request;
+        Mail::to('biuro@danfit.pl')->send(new EmailContactForm($request));
+
+        return response()->json(['message' => 'Email sent successfully']);
+    }
 }
