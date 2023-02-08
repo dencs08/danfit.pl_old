4bf169a69f589cb5a0a98325b83b6a305d049938
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-02-08 15:46:13 +0100

    add: Link to contact in PageInProgress

--- vue/src/views/PageInProgress.vue
+++ vue/src/views/PageInProgress.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="h-screen grid place-content-center text-center">
-        <h3 class="mb-4">Podstrona <TextGradient>aktualnie w budowie!</TextGradient>
+        <h3 class="">Podstrona <TextGradient>aktualnie w budowie!</TextGradient>
         </h3>
+        <h2 class="mb-10">
+            <Link link="Contact">Zarezerwuj trening przez telefon</Link>
+        </h2>
         <Link link="Start" :underline="true">Powrót na strone główną</Link>
     </div>
 </template>
