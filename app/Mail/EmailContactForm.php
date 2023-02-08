d7e78abfcc016c6eb2e4b6c21bb2396e4474ae06
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-02-08 15:48:13 +0100

    update: Mail contents

--- app/Mail/EmailContactForm.php
+++ app/Mail/EmailContactForm.php
@@ -13,45 +13,26 @@ class EmailContactForm extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $mailData;
+    public function __construct($data)
     {
-        //
+        $this->mailData = $data;
     }
 
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
     public function envelope()
     {
         return new Envelope(
-            subject: 'Email Contact Form',
+            subject: 'New email received from contactForm on danfit',
         );
     }
 
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
     public function content()
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails/email-user-form-email',
         );
     }
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
     public function attachments()
     {
         return [];
