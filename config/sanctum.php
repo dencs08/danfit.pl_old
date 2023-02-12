c4d2f3417c68421fbd58579de81d9f9c9f7f1d55
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-02-12 22:04:01 +0100

    update: laravel config for cors policies

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,localhost:8000,localhost:5173,danfit.pl,api.danfit.pl,https://api.danfit.pl,https://danfit.pl',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
