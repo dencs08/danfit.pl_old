f272911c565a5f21ab64d4b7239f1f07cc08a580
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-03-10 13:26:00 +0100

    add: nuxt /layouts

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+    <Navbar />
+    <section class="overflow-hidden relative">
+        <router-view v-slot="{ Component }">
+            <transition name="fade" mode="out-in">
+                <component :is="Component" />
+            </transition>
+        </router-view>
+    </section>
+</template>
+
+<script lang="ts">
+
+export default {
+    data() {
+        return {
+            // racket: racket,
+            // ball: ball
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.250s ease-out;
+}
+</style>
