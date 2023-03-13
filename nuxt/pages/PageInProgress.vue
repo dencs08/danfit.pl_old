33346d53c624fa7ab060f17469107c10129173da
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-03-13 15:44:20 +0100

    fix: PageInProgress Link > LinkCustom

--- nuxt/pages/PageInProgress.vue
+++ nuxt/pages/PageInProgress.vue
@@ -3,9 +3,9 @@
         <h3 class="">Podstrona <TextGradient>aktualnie w budowie!</TextGradient>
         </h3>
         <h2 class="mb-10">
-            <Link link="Contact">Zarezerwuj trening przez telefon</Link>
+            <LinkCustom link="Contact">Zarezerwuj trening przez telefon</LinkCustom>
         </h2>
-        <Link link="Start" :underline="true">Powrót na strone główną</Link>
+        <LinkCustom link="Start" :underline="true">Powrót na strone główną</LinkCustom>
     </div>
 </template>
 <script lang="ts">
