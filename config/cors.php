cf8fe39b4a72ef8bfa4df50d805fd0001d7c0193
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-03-14 17:08:47 +0100

    fix: CORS & sanctum stateful domains

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8000', 'https://danfit.pl','https://api.danfit.pl', 'http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:8000', 'https://danfit.pl','https://api.danfit.pl', 'http://localhost:5173', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
