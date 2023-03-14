c4a4d3bc47913c1a9ab084b950b423fcc5dee0ce
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-03-14 17:08:57 +0100

    remove: redundant div in app.vue

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+</script>
 
 <style lang="scss">
 @import './assets/scss/app.scss';
