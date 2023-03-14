eb65595edb261b4c59b9fc46f9cf10d65a737044
dencs08 <59261630+dencs08@users.noreply.github.com>
2023-03-14 17:07:04 +0100

    add: nuxt cofig headlessUI, runtime apibase

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    build: {},
+    modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-headlessui"],
 
-    modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+    headlessui: {
+        prefix: "Headless",
+    },
+
+    runtimeConfig: {
+        public: {
+            apiBase: process.env.BASE_API_URL,
+        },
+    },
 });
